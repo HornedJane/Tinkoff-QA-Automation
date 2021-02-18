a93625efb7476fda2c86d834b7f2c8efc2e872c6
HornedJanesStuff <dmitrykazakovdev@gmail.com>
2021-02-18 15:03:36 +0300

    15.02.2021 home assignment

---
+++ ElephantTester/src/main/java/Thanos_sort_main.java
@@ -0,0 +1,75 @@
+import java.util.Arrays;
+
+public class Thanos_sort_main {
+
+    public static double countMean(int lArrayIndex, int rArrayIndex, int[] array) {
+
+        double sum = 0;
+        double mean;
+
+        for (int i = lArrayIndex; i <= rArrayIndex; i++) {
+            sum += array[i];
+        }
+        mean = sum / (rArrayIndex - lArrayIndex + 1);
+        return mean;
+    }
+
+    public static void sortThanos(int lArrayIndex, int rArrayIndex, int[] array) {
+
+        if (lArrayIndex < rArrayIndex) {
+
+            double mean = countMean(lArrayIndex, rArrayIndex, array);
+            int[] bucket = new int[rArrayIndex - lArrayIndex + 1];
+            int lBucketIndex = 0;
+            int rBucketIndex = rArrayIndex - lArrayIndex;
+
+            for (int i = lArrayIndex; i <= rArrayIndex; i++) {
+                if (array[i] > mean) {
+                    bucket[rBucketIndex] = array[i];
+                    rBucketIndex--;
+                }
+                else {
+                    bucket[lBucketIndex] = array[i];
+                    lBucketIndex++;
+                }
+            }
+
+            for (int i = lArrayIndex; i <= rArrayIndex; i++) {
+                array[i] = bucket[i - lArrayIndex];
+            }
+
+            int lBucketIndexInArray = lBucketIndex + lArrayIndex - 1;
+            int rBucketIndexInArray = rBucketIndex + lArrayIndex + 1;
+
+            if (lBucketIndexInArray < rArrayIndex) {
+                sortThanos(lArrayIndex, lBucketIndexInArray, array);
+                sortThanos(rBucketIndexInArray, rArrayIndex, array);
+            }
+        }
+    }
+
+    public static int[] snapMyFingers(int[] array) {
+
+        int[] sortedArray = Arrays.copyOf(array, array.length);
+
+        sortThanos(0, sortedArray.length - 1, sortedArray);
+
+        return sortedArray;
+    }
+
+    public static void main(String[] args) {
+
+        int[] input = {2, 4, 5, -3, 23, 8, 25, -28, -3, 7}; //input hardcoded here!
+
+        int[] sortedInput = snapMyFingers(input);
+
+        for (int elem : input) {
+            System.out.print(elem + " ");
+        }
+        System.out.println("\n*snap*");
+        for (int elem : sortedInput) {
+            System.out.print(elem + " ");
+        }
+        System.out.println("\nPerfectly sorted, as all arrays should be");
+    }
+}
---
+++ ElephantTester/src/test/java/Thanos_sort_test.java
@@ -0,0 +1,86 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Thanos_sort_test {
+
+    @Test
+    public void positivesSortTest() {
+    int[] array = {2, 3, 1, 5, 4};
+    int[] expectedResult = {1, 2, 3, 4, 5};
+    int[] result = Thanos_sort_main.snapMyFingers(array);
+        Assertions.assertArrayEquals(expectedResult, result);
+    }
+
+    @Test
+    public void negativesSortTest() {
+        int[] array = {-2, -3, -1, -5, -4};
+        int[] expectedResult = {-5, -4, -3, -2, -1};
+        int[] result = Thanos_sort_main.snapMyFingers(array);
+        Assertions.assertArrayEquals(expectedResult, result);
+    }
+
+    @Test
+    public void integersSortTest() {
+        int[] array = {0, -5, 2, 14, -6, -8, 12, -2};
+        int[] expectedResult = {-8, -6, -5, -2, 0, 2, 12, 14};
+        int[] result = Thanos_sort_main.snapMyFingers(array);
+        Assertions.assertArrayEquals(expectedResult, result);
+    }
+
+    @Test
+    public void oddLengthSortTest() {
+        int[] array = {0, -5, 2, 14, -6};
+        int[] expectedResult = {-6, -5, 0, 2, 14};
+        int[] result = Thanos_sort_main.snapMyFingers(array);
+        Assertions.assertArrayEquals(expectedResult, result);
+    }
+
+    @Test
+    public void evenLengthSortTest() {
+        int[] array = {0, -5, 2, 14, -6, -8};
+        int[] expectedResult = {-8, -6, -5, 0, 2, 14};
+        int[] result = Thanos_sort_main.snapMyFingers(array);
+        Assertions.assertArrayEquals(expectedResult, result);
+    }
+
+    @Test
+    public void equalsSortTest() {
+        int[] array = {2, -2, -2, 2, -2, 0, 2, -2, -2};
+        int[] expectedResult = {-2, -2, -2, -2, -2, 0, 2, 2, 2};
+        int[] result = Thanos_sort_main.snapMyFingers(array);
+        Assertions.assertArrayEquals(expectedResult, result);
+    }
+
+    @Test
+    public void allEqualsSortTest() {
+        int[] array = {-13, -13, -13, -13};
+        int[] expectedResult = {-13, -13, -13, -13};
+        int[] result = Thanos_sort_main.snapMyFingers(array);
+        Assertions.assertArrayEquals(expectedResult, result);
+    }
+
+    @Test
+    public void singleSortTest() {
+        int[] array = {7};
+        int[] expectedResult = {7};
+        int[] result = Thanos_sort_main.snapMyFingers(array);
+        Assertions.assertArrayEquals(expectedResult, result);
+    }
+
+    @Test
+    public void emptySortTest() {
+        int[] array = {};
+        int[] expectedResult = {};
+        int[] result = Thanos_sort_main.snapMyFingers(array);
+        Assertions.assertArrayEquals(expectedResult, result);
+    }
+
+    //int -2,147,483,648 to 2,147,483,647
+    @Test
+    public void criticalValuesSortTest() {
+        int[] array = {-2147483648, 2147483647, 2147483647, -2147483648, -2147483648};
+        int[] expectedResult = {-2147483648, -2147483648, -2147483648, 2147483647, 2147483647};
+        int[] result = Thanos_sort_main.snapMyFingers(array);
+        Assertions.assertArrayEquals(expectedResult, result);
+    }
+}
