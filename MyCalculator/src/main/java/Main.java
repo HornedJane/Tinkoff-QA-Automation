7646452a3feb6f108420d0ac593b7cc8e747b435
HornedJanesStuff <dmitrykazakovdev@gmail.com>
2021-02-27 14:20:26 +0300

    18.02.2021 home assignment

--- MyCalculator/src/main/java/Main.java
+++ MyCalculator/src/main/java/Main.java
@@ -1,14 +1,12 @@
 import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class Main {
 
     public static void main(String[] args) {
 
         String inputExpression;
-        String operator;
-        double a;
-        double b;
-        Operation solver;
 
         System.out.println("This calculator can do binary operations\n" +
                 "available operators: +, -, *, /, ^\n");
@@ -17,15 +15,25 @@ public class Main {
 
         while (!inputExpression.equals("q")) {
 
-            String[] lex = inputExpression.split(" ");   // KLUDGE!
+            if (isCorrect(inputExpression)) {
 
-            a = Double.parseDouble(lex[0]);
-            b = Double.parseDouble(lex[2]);
-            operator = lex[1];
+                String operator;
+                double a;
+                double b;
+                Operation solver;
 
-            solver = getOperation(operator);
-            solver.execute(a, b);
+                String[] parsedExpression = parseExpression(inputExpression);
 
+                a = Double.parseDouble(parsedExpression[0]);
+                operator = parsedExpression[1];
+                b = Double.parseDouble(parsedExpression[2]);
+
+                solver = getOperation(operator);
+                solver.execute(a, b);
+
+            } else {
+                System.out.println("Invalid input. Expression pattern must be <num><operation><num>");
+            }
             inputExpression = getNewExpression();
         }
     }
@@ -36,7 +44,35 @@ public class Main {
 
         System.out.println("Type new expression or 'q' to quit");
 
-        return in.nextLine();
+        return in.nextLine().replace(" ", "");
+    }
+
+
+    public static boolean isCorrect(String expression) {
+
+        Pattern pattern = Pattern.compile("(-?\\d+(\\.\\d+)?)[+*/^-](-?\\d+(\\.\\d+)?)");
+        boolean result = false;
+
+        if (pattern.matcher(expression).matches()) {
+            result = true;
+        }
+
+        return result;
+    }
+
+    public static String[] parseExpression(String expression) {
+
+        String[] result = new String[3];
+        Pattern pattern = Pattern.compile("\\d[+*/^-](-?\\d)");
+        Matcher matcher = pattern.matcher(expression);
+
+        matcher.find();
+        int operationPosition = matcher.start() + 1;
+        result[0] = expression.substring(0, operationPosition);
+        result[1] = expression.substring(operationPosition, operationPosition + 1);
+        result[2] = expression.substring(operationPosition + 1);
+
+        return result;
     }
 
     public static Operation getOperation(String operator) {
